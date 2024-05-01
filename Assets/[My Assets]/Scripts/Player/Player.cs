40622009b1a3c3e2c0a7e4956e31879dc2f235cb
Ferchus <98918903+FerchusGames@users.noreply.github.com>
2024-05-01 15:20:00 -0600

    PlayerMoveState

--- Assets/[My Assets]/Scripts/Player/Player.cs
+++ Assets/[My Assets]/Scripts/Player/Player.cs
@@ -26,15 +26,15 @@ public class Player : MonoBehaviour, ICloned
     public Animator[] Animators { get; private set; } = new Animator[5];
 
     // State Control
-    public bool IsDead { get; private set; }
-    public bool IsFacingRight { get; private set; }
-    public bool IsJumping { get; private set; }
-    public bool IsJumpCut { get; private set; }
-    public bool IsJumpFalling { get; private set; }
+    public bool IsDead;
+    public bool IsFacingRight;
+    public bool IsJumping;
+    public bool IsJumpCut;
+    public bool IsJumpFalling;
 
     // Timers
-    public float LastOnGroundTime { get; private set; }
-    public float LastPressedJumpTime { get; private set; }
+    public float LastOnGroundTime;
+    public float LastPressedJumpTime;
 
     [field:Header("Acceleration")] [SerializeField]
     [field:SerializeField] public float RunMaxSpeed { get; private set; } = default;
@@ -123,15 +123,29 @@ public class Player : MonoBehaviour, ICloned
         UpdateTimers();
         HandleInput();
         GroundCheck();
-        JumpChecks();
-        GravityShifts();
         SetAnimatorParameters();
     }
 
     private void FixedUpdate()
     {
         _stateMachine.CurrentPlayerState.PhysicsUpdate();
-        Run();
+    }
+    
+    private void CheckDirectionToFace(bool isMovingRight)
+    {
+        if (isMovingRight != IsFacingRight)
+        {
+            Turn();
+        }
+    }
+
+    private void Turn()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+
+        IsFacingRight = !IsFacingRight;
     }
 
     #region INPUT HANDLER
@@ -157,175 +171,15 @@ public class Player : MonoBehaviour, ICloned
     }
 
     #endregion
-
-    #region RUN METHODS
-
-    private void Run()
-    {
-        float targetSpeed = MoveInput.x * RunMaxSpeed;
-
-        #region CALCULATING ACCELERATION RATE
-
-        float accelerationRate;
-
-        // Our acceleration rate will differ depending on if we are trying to accelerate or if we are trying to stop completely.
-        // It will also change if we are in the air or if we are grounded.
-
-        if (LastOnGroundTime > 0)
-        {
-            accelerationRate = (Mathf.Abs(targetSpeed) > 0.01f) ? RunAccelerationRate : RunDecelerationRate;
-        }
-
-        else
-        {
-            accelerationRate = (Mathf.Abs(targetSpeed) > 0.01f)
-                ? RunAccelerationRate * AirAccelerationMultiplier
-                : RunDecelerationRate * AirDecelerationMultiplier;
-        }
-
-        #endregion
-
-        #region ADD BONUS JUMP APEX ACCELERATION
-
-        if ((IsJumping || IsJumpFalling) && Mathf.Abs(PlayerRigidbody2D.velocity.y) < JumpHangTimeThreshold)
-        {
-            accelerationRate *= JumpHangAccelerationMultiplier;
-            targetSpeed *= JumpHangMaxSpeedMultiplier;
-        }
-
-        #endregion
-
-        float speedDifference = targetSpeed - PlayerRigidbody2D.velocity.x;
-
-        float movement = speedDifference * accelerationRate;
-
-        PlayerRigidbody2D.AddForce(movement * Vector2.right, ForceMode2D.Force);
-    }
-
-    private void CheckDirectionToFace(bool isMovingRight)
-    {
-        if (isMovingRight != IsFacingRight)
-        {
-            Turn();
-        }
-    }
-
-    private void Turn()
-    {
-        Vector3 scale = transform.localScale;
-        scale.x *= -1;
-        transform.localScale = scale;
-
-        IsFacingRight = !IsFacingRight;
-    }
-
-    #endregion
-
-    #region JUMP CHECKS
-
-    private void JumpChecks()
-    {
-        JumpingCheck();
-        JumpCutCheck();
-
-        if (CanJump() && LastPressedJumpTime > 0)
-        {
-            IsJumping = true;
-            IsJumpCut = false;
-            IsJumpFalling = false;
-            Jump();
-        }
-    }
-
-    private void JumpingCheck()
-    {
-        if (IsJumping && PlayerRigidbody2D.velocity.y < 0)
-        {
-            IsJumping = false;
-
-            IsJumpFalling = true;
-        }
-    }
-
-    private void JumpCutCheck()
-    {
-        if (LastOnGroundTime > 0 && !IsJumping)
-        {
-            IsJumpCut = false;
-            IsJumpFalling = false; // Logic failure in the original script?
-        }
-    }
-
-    #endregion
-
-    #region JUMP METHODS
-
-    private void Jump()
-    {
-        JumpResetTimers();
-
-        float force = JumpForce;
-
-        if (PlayerRigidbody2D.velocity.y < 0)
-        {
-            force -= PlayerRigidbody2D.velocity.y; // To always jump the same amount.
-        }
-
-        PlayerRigidbody2D.AddForce(Vector2.up * force, ForceMode2D.Impulse);
-    }
-
-    private void JumpResetTimers()
-    {
-        LastPressedJumpTime = 0;
-        LastOnGroundTime = 0;
-    }
-
-    #endregion
-
+    
     #region GRAVITY
 
-    private void SetGravityScale(float gravityScale)
+    public void SetGravityScale(float gravityScale)
     {
         PlayerRigidbody2D.gravityScale = gravityScale;
     }
 
-    private void GravityShifts()
-    {
-        // Make player fall faster if holding down S
-        if (PlayerRigidbody2D.velocity.y < 0 && MoveInput.y < 0)
-        {
-            SetGravityScale(GravityScale * FallGravityMultiplier);
-            FallSpeedCap(MaxFastFallSpeed);
-        }
-
-        // Scale gravity up if jump button released
-        else if (IsJumpCut)
-        {
-            SetGravityScale(GravityScale * JumpCutGravityMultiplier);
-            FallSpeedCap(MaxFallSpeed);
-        }
-
-        // Higher gravity when near jump height apex
-        else if ((IsJumping || IsJumpFalling) && Mathf.Abs(PlayerRigidbody2D.velocity.y) < JumpHangTimeThreshold)
-        {
-            SetGravityScale(GravityScale * JumpHangGravityMultiplier);
-        }
-
-        // Higher gravity if falling
-        else if (PlayerRigidbody2D.velocity.y < 0)
-        {
-            SetGravityScale(GravityScale * FallGravityMultiplier);
-            FallSpeedCap(MaxFallSpeed);
-        }
-
-        // Reset gravity
-        else
-        {
-            SetGravityScale(GravityScale);
-        }
-    }
-
-    private void FallSpeedCap(float fallSpeedMaxValue)
+    public void FallSpeedCap(float fallSpeedMaxValue)
     {
         PlayerRigidbody2D.velocity = new Vector2(PlayerRigidbody2D.velocity.x,
             Mathf.Max(PlayerRigidbody2D.velocity.y, -fallSpeedMaxValue));
@@ -364,12 +218,12 @@ public class Player : MonoBehaviour, ICloned
 
     #region CHECK METHODS
 
-    private bool CanJump()
+    public bool CanJump()
     {
         return LastOnGroundTime > 0 && !IsJumping;
     }
 
-    private bool CanJumpCut()
+    public bool CanJumpCut()
     {
         return IsJumping && PlayerRigidbody2D.velocity.y > 0;
     }
@@ -382,22 +236,6 @@ public class Player : MonoBehaviour, ICloned
     {
         Animators.SetFloat(_ahSpeed, Mathf.Abs(MoveInput.x));
         Animators.SetBool(_ahIsJumping, IsJumping || IsJumpFalling);
-        
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            IsDead = !IsDead;
-            Animators.SetBool(_ahIsDead, IsDead);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            Animators.SetTrigger(_ahShoot);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Mouse1))
-        {
-            Animators.SetTrigger(_ahMelee);
-        }
     }
     
     #endregion
